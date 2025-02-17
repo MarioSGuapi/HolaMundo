e5fc4d4d7e86f37de968a75d4130e5dabe0adb75
mario guapi <msg98@hotmail.com>
2025-02-16 23:27:58 -0500

    Se agrego una linea de comando no sirve

--- src/com/mariogit/PersonaRegistro.java
+++ src/com/mariogit/PersonaRegistro.java
@@ -4,6 +4,7 @@ public record PersonaRegistro(String nombre, String apellidos) {
 
     @Override
     public String toString() {
+        System.out.println("Esta instrucion es in");
         return "%s %s".formatted(nombre, apellidos);
     }
 }
